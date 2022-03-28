819befa0ac34c7bf74c4cd3ea4ea69cbf961bf1b
Matimed <mcarazomedley@gmail.com>
2022-03-28 09:20:01 -0300

    Maze implemented.

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -2,5 +2,6 @@ from src.model.biomes_manager import BiomesManager
 from src.model.innocent import Innocent
 from src.model.player import Player
 from src.model.entity_factory import EntityFactory
+from src.model.maze import Maze
 from src.model.world import World
 from src.model.logic import Logic
