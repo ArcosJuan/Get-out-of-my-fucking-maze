67f9ed008c48a3a2fd5fb6405bc81284a7c2e9c8
Matimed <mcarazomedley@gmail.com>
2022-03-28 09:19:47 -0300

    Innocents implemented.

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,4 +1,6 @@
 from src.model.biomes_manager import BiomesManager
+from src.model.innocent import Innocent
 from src.model.player import Player
+from src.model.entity_factory import EntityFactory
 from src.model.world import World
 from src.model.logic import Logic
