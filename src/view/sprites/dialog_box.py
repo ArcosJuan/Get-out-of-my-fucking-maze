f2f86346ba5d3e5f34c728ca3a61684bdfde8f54
ArcosJuan <arcos0juan@gmail.com>
2022-04-03 19:23:21 -0300

    Add text printing in DialogBoxSprite.
    DialogBoxSprite can recive a text and render it in itself.

--- src/view/sprites/dialog_box.py
+++ src/view/sprites/dialog_box.py
@@ -1,26 +1,46 @@
 import pygame as pg
 from src.references.images import DIALOG
 from src.view.window import Window
+from src.view.sprites import TextBoxSprite
 
 
 class DialogBoxSprite:
-    def __init__(self, min_size = 20, max_size = 38, margins = (10, 10)):
+    def __init__(self, min_size = 20, max_size = 38, box_margins = (10, 10), text_height_percentage=25, text_margins_percentage=(3,10), text_color=(255,0,0), text_line_spacing=0):
         """ Creates the dialog box image and act as the
             mediator to the dialog box image.
         """
+        # BOX INITIALIZATION:
 
         # Left, right and bottom margins (NOT top margins).
         # The horizontal margins may vary according 
         # to the proportions of the box so it's more like the 
         # min size of horizontal margins :).
         self.box_pieces = {"topleft": DIALOG["TOPLEFT"], "top": DIALOG["TOP"], "center": DIALOG["CENTER"]}
-        self.margins = margins 
+        self.box_margins = box_margins 
 
         self.validate_window_size()
         self.set_box_proportions(min_size, max_size)
         
-        self.box_image = self.get_box_image()
-        self.box_rect = self.get_box_rect(self.box_image)
+        self.box_image = self._create_box_image()
+        self.box_rect = self._create_box_rect(self.box_image)
+
+        # TEXT INITIALIZATION:
+
+        # Text properties:
+        self.text_p = {
+            'height_percentage':text_height_percentage,
+            'margins_percentage': text_margins_percentage,
+            'color': text_color,
+            'line_spacing': text_line_spacing
+        }
+
+        # Text itself:
+        self.text_rows = []
+
+        self.text_area = self._create_text_area(self.text_p['margins_percentage'])
+        TextBoxSprite.set_font_height(self.box_image.get_size()[1] * self.text_p['height_percentage'] / 100)
+        
+        
 
 
     def set_box_proportions(self, min_size, max_size):
@@ -54,18 +74,18 @@ class DialogBoxSprite:
         box_min_size[0] += self.box_pieces["top"].get_size()[0] * 3
         box_min_size[1] += self.box_pieces["top"].get_size()[1] * 2
 
-        if Window().resolution[0] < box_min_size[0] + self.margins[0] \
-            or Window().resolution[1] // 3 < box_min_size[1] + self.margins[1]:
+        if Window().resolution[0] < box_min_size[0] + self.box_margins[0] \
+            or Window().resolution[1] // 3 < box_min_size[1] + self.box_margins[1]:
             raise AssertionError(
             f"Current Window resolution cannot show dialogue box properly.\n"
                 + "Window witdh needs to be larger or equal than"
-                + f" {box_min_size[0] + self.margins[0]} and the"
+                + f" {box_min_size[0] + self.box_margins[0]} and the"
                 + "third part of the height larger or equal than"
-                + f" {box_min_size[1] + self.margins[1]}."
+                + f" {box_min_size[1] + self.box_margins[1]}."
             )
 
 
-    def get_box_image(self):
+    def _create_box_image(self):
         """ Build the dialog box image with the box_pieces 
             and according to the window size.
         """
@@ -76,9 +96,9 @@ class DialogBoxSprite:
         corner_size = self.box_pieces["topleft"].get_size()
 
         box_size = [0,0]
-        box_size[0] += ((Window().resolution[0]  - self.margins[0] * 2) // corner_size[0]) * corner_size[0]
+        box_size[0] += ((Window().resolution[0]  - self.box_margins[0] * 2) // corner_size[0]) * corner_size[0]
 
-        box_size[1] += ((Window().resolution[1] // 3 - self.margins[1]) // corner_size[1]) * corner_size[1]
+        box_size[1] += ((Window().resolution[1] // 3 - self.box_margins[1]) // corner_size[1]) * corner_size[1]
 
         box_image = pg.Surface(box_size)
 
@@ -119,20 +139,133 @@ class DialogBoxSprite:
         return box_image
 
 
-    def get_box_rect(self, edges_image):
+    def _create_box_rect(self, edges_image):
         """ Creates the dialog box rect and position it on the screen.
         """
 
         box_rect = self.box_image.get_rect()
 
         # Position the rect on the window.  
-        box_rect = (
+        box_rect.topleft = (
             (Window().resolution[0]  - self.box_image.get_size()[0]) // 2,
-            Window().resolution[1] - self.box_image.get_size()[1] - self.margins[1]
+            Window().resolution[1] - self.box_image.get_size()[1] - self.box_margins[1]
             )
         
         return box_rect
 
 
-    def draw(self, surface = None):
+    def _create_text_area(self, margin_percentage):
+        """ Creates the rect where the text will be drawn.
+            Recives: 
+                margin_percentage<tuple(<int>,<int>)>
+            
+            Returns:
+                text_area<pygame.Rect>
+        """
+        
+        box_size = self.box_image.get_size()
+
+        width = box_size[0] - box_size[0] * margin_percentage[0] / 100
+        height = box_size[1] - box_size[1] * margin_percentage[1] / 100
+        
+        text_area = pg.Rect(0, 0, width, height)
+        text_area.center = self.box_rect.center
+        return text_area
+
+
+    def draw(self, event, surface = None):
         (surface if surface else Window().surface).blit(self.box_image, self.box_rect)
+        if self.text_rows: 
+            for text_box in self.text_rows:
+                text_box.draw()
+
+
+    def set_text(self, text):
+        """ Set the text that will be shown.
+        """
+
+        self.text_rows = []
+
+        for row in range(0, len(text)):
+            self.text_rows.append(TextBoxSprite(text[row], self.text_p['color']))
+            if row == 0:
+                self.text_rows[0].get_rect().topleft = self.text_area.topleft
+            else: 
+                x = self.text_rows[row-1].get_rect().bottomleft[0]
+                y = self.text_rows[row-1].get_rect().bottomleft[1] + self.text_p['line_spacing']
+                self.text_rows[row].get_rect().topleft =  (x,y)
+
+
+    def _cut_text_horizontally(self, text):
+        """ Given a text, it is cut in such a way that each row can
+            be printed inside the text_area.
+            Recives: 
+        """
+
+        cut_text = []
+        text_width = TextBoxSprite.get_font().size(text)[0]
+        if TextBoxSprite.get_font().size(text)[0] > self.text_area.width:
+            line_width = self.text_area.width *  100 / text_width
+            line_width = int(line_width * len(text) // 100)
+
+            while True:
+                if TextBoxSprite.get_font().size(text[:line_width])[0] + TextBoxSprite.get_font().size('-')[0] >= self.text_area.width:
+                    line_width -= 1
+                else: break
+
+            
+            if self._hyphen_validation(text, line_width):
+                cut_text.append(text[:line_width] + '-')
+            else: cut_text.append(text[:line_width])
+            
+            cut_text.extend(self._cut_text_horizontally(text[line_width:].strip()))
+
+            return cut_text  
+
+        else: 
+            return [text]
+            
+
+    def _hyphen_validation(self, text, line_width):
+        """ The conditions used to know if the cut text needs
+            a hyphen in the next row.
+        """
+
+        conditions = (
+            text[line_width - 1] != ' '
+            and text[line_width] != ' ' 
+            and text[line_width - 1] != '-' 
+            and text[line_width - 1] != ','
+            )
+
+        return conditions
+
+
+    def _cut_text_vertically(self, text):
+        """ Given a list of text rows, it cuts them into groups where
+            each group can fit in the text_area.
+        """
+
+        rows = self.text_area.height // (TextBoxSprite.get_font_height() + self.text_p['line_spacing'])
+        cut_text = []
+        while text:
+            group = []
+            for i in range(rows):
+                if len(text) != 0:
+                    group.append(text.pop(0)) 
+
+                else: break
+
+            cut_text.append(group)
+        return cut_text
+
+    
+    def adjust_text(self, text):
+        """ Adjust the text to fit the text_area. If the text is too big,
+            it returns groups of text where each group can fit
+            in the textarea.
+        """
+
+        horizontally_adjusted_text = self._cut_text_horizontally(text)
+        adjusted_text = self._cut_text_vertically(horizontally_adjusted_text)
+        return adjusted_text
