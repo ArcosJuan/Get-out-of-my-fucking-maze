ca0aca4e8e70f0864906bd0ff8a359caea849207
ArcosJuan <arcos0juan@gmail.com>
2022-04-06 15:36:34 -0300

    Add dialog mode in pygame controller.

--- src/controller/pygame_controller.py
+++ src/controller/pygame_controller.py
@@ -1,6 +1,8 @@
 import pygame as pg
 from src.events import ArrowKey
 from src.events import Click
+from src.events import ChangeDialogMode
+from src.events import PassDialog
 from src.events import Quit
 from src.events import Tick
 from src.events import Wheel
@@ -14,6 +16,8 @@ class PygameController:
 
     def __init__(self):
         Ed.add(Tick, self.iterate_events)
+        Ed.add(ChangeDialogMode, self.change_dialog_mode)
+        self.dialog_mode = False
         self.arrow_keys_pressed = dict()
 
     def iterate_events(self, event):
@@ -21,29 +25,35 @@ class PygameController:
             if event.type == pg.QUIT:
                 Ed.post(Quit())
 
-            elif event.type == pg.KEYUP:
-                if event.key in self.arrow_keys_pressed:
-                    self.arrow_keys_pressed.pop(event.key)
-                if event.key == pg.K_n:
-                    Ed.post(ShiftEnded())
+            if not self.dialog_mode:
+                if event.type == pg.KEYUP:
+                    if event.key in self.arrow_keys_pressed:
+                        self.arrow_keys_pressed.pop(event.key)
 
-            elif event.type == pg.KEYDOWN:
-                if event.key == pg.K_UP: 
-                    self.arrow_keys_pressed[event.key] = ArrowKey(y=1)
-                if event.key == pg.K_DOWN: 
-                    self.arrow_keys_pressed[event.key] = ArrowKey(y=-1)
-                if event.key == pg.K_RIGHT: 
-                    self.arrow_keys_pressed[event.key] = ArrowKey(x=1)
-                if event.key == pg.K_LEFT: 
-                    self.arrow_keys_pressed[event.key] = ArrowKey(x=-1)
+                elif event.type == pg.KEYDOWN:
+                    if event.key == pg.K_UP: 
+                        self.arrow_keys_pressed[event.key] = ArrowKey(y=1)
+                    if event.key == pg.K_DOWN: 
+                        self.arrow_keys_pressed[event.key] = ArrowKey(y=-1)
+                    if event.key == pg.K_RIGHT: 
+                        self.arrow_keys_pressed[event.key] = ArrowKey(x=1)
+                    if event.key == pg.K_LEFT: 
+                        self.arrow_keys_pressed[event.key] = ArrowKey(x=-1)
+                    
+
+                elif event.type == pg.MOUSEBUTTONUP:
+                    Ed.post(Click(event.pos, event.button))
                 
+            else: 
+                if event.type == pg.KEYUP:
+                    if event.key == pg.K_n:
+                        Ed.post(PassDialog())
 
-            elif event.type == pg.MOUSEBUTTONUP:
-                Ed.post(Click(event.pos, event.button))
 
-            elif event.type == pg.MOUSEWHEEL:
-                Ed.post(Wheel(event.y))
                 
-
         if self.arrow_keys_pressed: 
             [Ed.post(event) for event in self.arrow_keys_pressed.values()]
+
+    
+    def change_dialog_mode(self, event):
+        self.dialog_mode = not self.dialog_mode
