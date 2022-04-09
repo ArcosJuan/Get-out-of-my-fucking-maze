40764f74b2957bcd3a9c73b6d91d0dfa8ab3d9c8
ArcosJuan <arcos0juan@gmail.com>
2022-04-09 16:58:05 -0300

    Fix Popup menus implementation.

--- src/view/scenes/main_menu.py
+++ src/view/scenes/main_menu.py
@@ -1,6 +1,7 @@
 from src.events import EndScene
 from src.events import Tick
 from src.events import Click
+from src.events import Quit
 from src.view.scenes import Scene
 from src.controller.event_dispatcher import EventDispatcher as Ed
 from src.view.sprites.popup_menu import PopupMenu
@@ -17,36 +18,39 @@ class MainMenu(Scene):
         self.change_showing_menu("Main")
 
     def change_showing_menu(self, new_menu):
-        if self.resolution == (960,540):
-            if new_menu == "Main":
-                self.showing_menu = PopupMenu([("Play", EndScene(self.__class__)), ("Resolution", Wbm(self.change_showing_menu, "Resolution"))], (100, 100), max_size=40, text_height_percentage=10)
-        
-            if new_menu == "Resolution":
-                self.showing_menu = PopupMenu([("960x540", Wbm(self.set_window_resolution, (960,540))), ("600x900", Wbm(self.set_window_resolution, (600,900))), ("Back", Wbm(self.change_showing_menu, "Main"))], (100, 100), max_size=40, text_height_percentage=10)
+        if new_menu == "Main":
+            self.showing_menu = PopupMenu(
+                [
+                    ("Play", EndScene(self.__class__)),
+                    ("Resolution", Wbm(self.change_showing_menu, "Resolution")),
+                    ("Exit", Quit())
+                ],
+                (1/2, 1/2),
+                max_size=40,
+                txt_a_pct=1
+            )
+    
+        if new_menu == "Resolution":
+            self.showing_menu = PopupMenu(
+                [
+                    ("960x540", Wbm(self.set_window_resolution, (960,540))),
+                    ("600x900", Wbm(self.set_window_resolution, (600,900))), 
+                    ("300x900", Wbm(self.set_window_resolution, (900,300))), 
+                    ("Back", Wbm(self.change_showing_menu, "Main"))
+                ], 
+                    (1/2, 1/2),
+                    max_size=40,
+                    txt_a_pct=1
+            )
 
-        if self.resolution == (600,900):
-            if new_menu == "Main":
-                self.showing_menu = PopupMenu([("Play", EndScene(self.__class__)), ("Resolution", Wbm(self.change_showing_menu, "Resolution"))], (50, 50), max_size=40, text_height_percentage=5)
-        
-            if new_menu == "Resolution":
-                self.showing_menu = PopupMenu([("960x540", Wbm(self.set_window_resolution, (960,540))), ("600x900", Wbm(self.set_window_resolution, (600,900))), ("Back", Wbm(self.change_showing_menu, "Main"))], (50, 50), max_size=40, text_height_percentage=5)
-
-
+    
     def set_window_resolution(self, resolution):
-        self.resolution = resolution
-        if resolution == (600,900):
-            self.showing_menu = PopupMenu([("960x540", Wbm(self.set_window_resolution, (960,540))), ("600x900", Wbm(self.set_window_resolution, (600,900))), ("Back", Wbm(self.change_showing_menu, "Main"))], (50, 50), max_size=40, text_height_percentage=5)
-        
-        if resolution == (960,540):
-            self.showing_menu = PopupMenu([("960x540", Wbm(self.set_window_resolution, (959,530))), ("600x900", Wbm(self.set_window_resolution, (600,900))), ("Back", Wbm(self.change_showing_menu, "Main"))], (100, 100), max_size=40, text_height_percentage=10)
-
         Window().resolution = resolution
-
+        Window().background = (0,0,0)
 
         
     def __del__(self): 
         self.showing_menu.__del__()
-
 
 
     def update(self, event):
