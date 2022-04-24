0dc92913aa57e3fdb50f9530be3d4bb26a2e447a
Matimed <mcarazomedley@gmail.com>
2022-04-23 22:21:26 -0300

    Create dynamic resolution options in MainMenu.

--- src/view/scenes/main_menu.py
+++ src/view/scenes/main_menu.py
@@ -1,3 +1,4 @@
+import pygame as pg
 from lib.weak_bound_method import WeakBoundMethod as Wbm
 from src.controller import EventDispatcher as Ed
 from src.events import Click
@@ -7,16 +8,33 @@ from src.events import Quit
 from src.view import Window
 from src.view.scenes import Scene
 from src.view.sprites import PopupMenu
+from src.view.sprites import Sprite
 
 
 class MainMenu(Scene):
     def __init__(self):
         super().__init__()
         Ed.add(Tick, Scene.window.update)
-        self.resolution = (960,540)
         Ed.add(Tick, self.update)
+        self.resolutions = self.create_resolutions()
+        self.set_window_resolution(self.resolutions[1])
         self.showing_menu = None
         self.change_showing_menu("Main")
+
+    def create_resolutions(self):
+        max_resolution = Window().get_display_size()
+        sprite_size = Sprite.get_actual_size()
+        resolutions = [
+            (int(max_resolution[0]/i), int(max_resolution[1]/i)) 
+            for i in range(1,4)
+        ]
+        
+        # We calculate the optimal resolutions to avoid black borders on the screen.
+        return [
+            (int(res[0]/sprite_size)*sprite_size, int(res[1]/sprite_size)*sprite_size) 
+            for res in resolutions
+        ]
+        
 
     def change_showing_menu(self, new_menu):
         if new_menu == "Main":
@@ -28,20 +46,30 @@ class MainMenu(Scene):
                 ],
                 (1/2, 1/2),
                 max_size=40,
-                txt_a_pct=1
+                txt_to_pct=1,
+                text_active_color=(0,255,0),
             )
     
-        if new_menu == "Resolution":
+        elif new_menu == "Resolution":
+            options = [(
+                f"{res[0]}x{res[1]}", 
+                Wbm(self.set_window_resolution, None, res)
+                ) for res in self.resolutions
+            ]
+        
+            options.extend([
+                ("Full Screen", Wbm(self.set_window_resolution, None, Window().get_display_size(), pg.FULLSCREEN)),
+                ("Back", Wbm(self.change_showing_menu, None, "Main")),
+            ])
+
+            current_resolution = self.resolutions.index(Window().get_resolution()) if Window().get_resolution() in self.resolutions else 0
             self.showing_menu = PopupMenu(
-                [
-                    ("960x540", Wbm(self.set_window_resolution, None, (960,540))),
-                    ("600x900", Wbm(self.set_window_resolution, None, (600,900))), 
-                    ("300x900", Wbm(self.set_window_resolution, None, (900,300))), 
-                    ("Back", Wbm(self.change_showing_menu, None, "Main"))
-                ], 
-                    (1/2, 1/2),
-                    max_size=40,
-                    txt_a_pct=1
+                options=options,
+                box_relative_pos=(1/2, 1/2),
+                max_size=40,
+                txt_to_pct=1,
+                text_active_color=(0,255,0),
+                initial_index=current_resolution,
             )
 
     
